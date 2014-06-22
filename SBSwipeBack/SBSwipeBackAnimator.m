5a9274418343ca1f2e547cde1f6129c4c834358e
c0ming <c0ming@qq.com>
2014-06-22 13:58:52 +0800

    add mask layer when transition

--- SBSwipeBack/SBSwipeBackAnimator.m
+++ SBSwipeBack/SBSwipeBackAnimator.m
@@ -12,6 +12,10 @@
 
 static CGFloat const SBShadowOpacityBegin = 0.75;
 static CGFloat const SBShadowOpacityEnd = 0.1;
+
+static CGFloat const SBOpacityBegin = 0.1;
+static CGFloat const SBOpacityEnd = 0.0;
+
 static CGFloat const SBTransitionDuration = 0.25;
 
 @implementation SBSwipeBackAnimator
@@ -37,18 +41,33 @@ static CGFloat const SBTransitionDuration = 0.25;
     fromViewCenter.x = width + width / 2.0;
     toViewCenter.x = width / 2.0;
 
+    // mask layer
+    CALayer *maskLayer = [CALayer layer];
+    maskLayer.frame = toViewController.view.bounds;
+    maskLayer.backgroundColor = [UIColor blackColor].CGColor;
+    maskLayer.opacity = SBOpacityBegin;
+    [toViewController.view.layer addSublayer:maskLayer];
+
     [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0.0 options:UIViewAnimationOptionCurveLinear animations: ^{
         fromViewController.view.center = fromViewCenter;
         toViewController.view.center = toViewCenter;
     } completion: ^(BOOL finished) {
+        [maskLayer removeFromSuperlayer];
+
         [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
     }];
 
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"shadowOpacity"];
-    animation.fromValue = [NSNumber numberWithFloat:SBShadowOpacityBegin];
-    animation.toValue = [NSNumber numberWithFloat:SBShadowOpacityEnd];
-    animation.duration = [self transitionDuration:transitionContext];
-    [fromViewController.view.layer addAnimation:animation forKey:@"shadowOpacity"];
+    CABasicAnimation *shadowOpacityAnimation = [CABasicAnimation animationWithKeyPath:@"shadowOpacity"];
+    shadowOpacityAnimation.fromValue = [NSNumber numberWithFloat:SBShadowOpacityBegin];
+    shadowOpacityAnimation.toValue = [NSNumber numberWithFloat:SBShadowOpacityEnd];
+    shadowOpacityAnimation.duration = [self transitionDuration:transitionContext];
+    [fromViewController.view.layer addAnimation:shadowOpacityAnimation forKey:@"shadowOpacity"];
+
+    CABasicAnimation *opacityAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    opacityAnimation.fromValue = [NSNumber numberWithFloat:SBOpacityBegin];
+    opacityAnimation.toValue = [NSNumber numberWithFloat:SBOpacityEnd];
+    opacityAnimation.duration = [self transitionDuration:transitionContext];
+    [maskLayer addAnimation:opacityAnimation forKey:@"opacity"];
 }
 
 - (void)addShadow:(UIView *)view {
